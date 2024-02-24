8abfdf2c199b3388381495c8f36d4469eff85c1f
Grandez <javier.gonzalez.grandez@gmail.com>
2024-02-24 17:37:31 +0100

    logger tests

--- tests/testthat/test-logger-console.R
+++ tests/testthat/test-logger-console.R
@@ -1,3 +1,4 @@
+
 test_that("Logger Default values", {
    logger = SDPLogger$new()
    expect_equal(logger$level,  0)
@@ -16,14 +17,4 @@ test_that("Logger Environment overrrided", {
    logger = SDPLogger$new(envvars="TEST", level=1, output=2)
    expect_equal(logger$level,  1)
    expect_equal(logger$output, 2)
-})
-test_that("Logger level not reached", {
-   logger = SDPLogger$new(level=1, output=1)
-   output = capture_output(logger$log(2, "Message"))
-   expect_equal(nchar(output), 0)
-})
-test_that("Logger level reached", {
-   logger = SDPLogger$new(level=3, output=1)
-   output = capture_output(logger$log(2, "Message"))
-   expect_gt(nchar(output), 0)
 })
